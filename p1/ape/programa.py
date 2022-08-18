8ea96afff8119cd489b55e8f03a03cea9ad0ade5
Johnner Yelcde <johnnercastrojy@gmail.com>
2022-08-18 15:05:03 -0300

    continuação do primeiro programinha

--- p1/ape/programa.py
+++ p1/ape/programa.py
@@ -1,3 +1,10 @@
-num = int(input('Digite um número inteiro: '))
-dobro = num*2
-print('O dobro de', num, 'é', dobro)
+#num = int(input('Digite um número inteiro: ')) 
+#dobro = num*2
+#print('O dobro de', num, 'é', dobro)
+
+anonasc = int(input('Diga o ano do seu nascimento: '))
+anoatual = int(input('Diga o ano atual: '))
+idade = anoatual - anonasc
+print(f'Você, que nasceu em {anonasc}, no ano de {anoatual} tem {idade} anos')
+#texto = 'Você, que nasceu em {}, em {} tem {} anos'
+#print(texto.format(anonasc, anoatual, idade))
