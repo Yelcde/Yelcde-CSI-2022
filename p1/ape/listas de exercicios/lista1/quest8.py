ff6788f98a2edd988ae53a3b841890b20f5fe46a
Yelcde <johnnercastrojy@gmail.com>
2022-08-19 13:56:13 -0300

    finalizando lista 1 de APE

---
+++ p1/ape/listas de exercicios/lista1/quest8.py
@@ -0,0 +1,6 @@
+nome = input('Qual o seu nome? ')
+nota1 = float(input('Sua primeira nota? '))
+nota2 = float(input('Sua segunda nota? '))
+nota3 = float(input('Sua terceira nota? '))
+media = (nota1+nota2+nota3)/3
+print(f'Aluno(a) {nome}, sua primeira nota é {nota1}, sua segunda nota é {nota2} e sua terceira nota é {nota3}, sua média é {media} ')
