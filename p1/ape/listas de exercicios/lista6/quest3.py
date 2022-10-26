b72afdd34ec1557f06ffa07b1f8fba8e5e699a12
Johnner Yelcde <johnnercastrojy@gmail.com>
2022-10-26 14:54:17 -0300

    continuação da lisa de Matriz

--- p1/ape/listas de exercicios/lista6/quest3.py
+++ p1/ape/listas de exercicios/lista6/quest3.py
@@ -1,35 +1,35 @@
-# import random
+import random
 
-# # leitura da ordem da matriz
+# leitura da ordem da matriz
 
-# ordem = int(input('Me dê o tamanho da matriz: '))
+ordem = int(input('Me dê o tamanho da matriz: '))
 
-# # criação da matriz com valores nulos
+# criação da matriz com valores nulos
 
-# a = [[None]*n for i in range(ordem)]
-# b = [[None]*n for i in range(ordem)]
+a = [[None]*n for i in range(ordem)]
+b = [[None]*n for i in range(ordem)]
 
-# # geração da matriz com valores aleatorios
+# geração da matriz com valores aleatorios
 
-# for i in range(n):
-#     for j in range(n):
-#         m[i][j] = random.randint(1, 20)
+for i in range(n):
+    for j in range(n):
+        m[i][j] = random.randint(1, 20)
 
-# # imprimir a matriz
+# imprimir a matriz
 
-# print('Matriz')
-# for i in range(n):
-#     for j in range(n):
-#         print(f'{m[i][j]:3}', end='')
-#     print()
+print('Matriz')
+for i in range(n):
+    for j in range(n):
+        print(f'{m[i][j]:3}', end='')
+    print()
 
-# # diagonal
+# diagonal
 
-# print('Diagonal')
-# for i in range(n):
-#     for j in range(n):
-#         if (i == j):
-#             print(f'{m[i][j]:3}', end='')
-#     print('', end='')
+print('Diagonal')
+for i in range(n):
+    for j in range(n):
+        if (i == j):
+            print(f'{m[i][j]:3}', end='')
+    print('', end='')
 
-# NÃO TERMINADA
+NÃO TERMINADA
