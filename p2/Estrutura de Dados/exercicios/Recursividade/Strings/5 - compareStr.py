fcd55f9aa62c60c3349e71a09e819023c280a0ab
Johnner Yelcde <johnnercastrojy@gmail.com>
2023-05-29 12:43:25 -0300

    Finalizando mais uma questão da lista de exercicio de ED

--- p2/Estrutura de Dados/exercicios/Recursividade/Strings/5 - compareStr.py
+++ p2/Estrutura de Dados/exercicios/Recursividade/Strings/5 - compareStr.py
@@ -1,26 +1,15 @@
 def compareStr(str1, str2):
-    global i
-    global j
     if len(str1) == 0 and len(str2) == 0:
-        return 
-    if len(str1) > 0: 
-        i += 1
-        compareStr(str1[1:], str2)
-    else:
-        j += 1
-        compareStr(str1, str2[1:])
-    return i and j
+        print('str1 é igual a str2')
+        return
+    elif len(str1) == 0:
+        print('str1 é menor a str2')
+        return
+    elif len(str2) == 0:
+        print('str1 é maior a str2')
+        return
+    compareStr(str1[1:], str2[1:])
 
 
 if __name__ == "__main__":
-    global i 
-    i = 0
-    global j
-    j = 0
     compareStr('Jyan', "Sammuel")
-    if i == j:
-        print('str1 é igual a str2')
-    elif i > j:
-        print('str1 é maior a str2')
-    elif i < j:
-        print('str1 é menor a str2')
