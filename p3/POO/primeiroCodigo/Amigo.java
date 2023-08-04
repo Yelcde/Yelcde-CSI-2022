a9f38755313b4c404d82e2b64d583921109d439c
Johnner Yelcde <johnnercastrojy@gmail.com>
2023-08-04 17:04:14 -0300

    Primeiro programa em Java

---
+++ p3/POO/primeiroCodigo/Amigo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Amigo {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Qual é o seu nome? ");
+        String nome1 = teclado.nextLine();
+        System.out.println(nome1+", de quem você é amigo? ");
+        String nome2 = teclado.nextLine();
+        System.out.println(nome1+" é amigo de "+nome2);
+    }
+}
