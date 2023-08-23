807495b8a06b6f9764a400dab5656e811d071c26
Johnner Yelcde <johnnercastrojy@gmail.com>
2023-08-23 16:56:19 -0300

    Script BD de Filmes
    Comecei a fazer esse script misturando os scripts de criação do banco com os códigos dos exercicios. Agora que separei estou enviando apenas o código de criação e população do banco Filmes.

--- p3/Banco de Dados II/Filmes/filme.sql
+++ p3/Banco de Dados II/Filmes/filme.sql
@@ -49,15 +49,7 @@ REFERENCES Artista;
 ALTER TABLE Personagem ADD CONSTRAINT FKPersonagem1Filme FOREIGN KEY(CodFILME)
 REFERENCES Filme;
 
--- 4. Verifique a estrutura das tabelas após sua criação: ARTISTA, FILME, CATEGORIA, ESTÚDIO e PERSONAGEM. Estão compatíveis com os modelos?
-
-select * from artista;
-select * from filme;
-select * from categoria;
-select * from estudio;
-select * from personagem;
-
--- 5. Rode os scripts de inserção dos dados de cada tabela, conforme scripts passados.
+-- Inserindo os dados
 
 insert into artista values(default,'Cameron Diaz',null,'USA','15/07/75');
 insert into artista values(default,'Julia Roberts',null,'USA','20/08/67');
@@ -66,6 +58,11 @@ insert into artista values(default,'Joaquin Phoenix',null,null,'06/04/72');
 insert into artista values(default,'Bradley Cooper',null,'USA','06/06/77');
 insert into artista values(default,'Tom Cruise',null,'USA','10/09/64');
 insert into artista values(default,'Rodrigo Santoro','Rio de Janeiro','Brasil','12/10/78');
+insert into artista values(default,'JungKook',null,'USA','15/07/75');
+insert into artista values(default,'V',null,'USA','20/08/67');
+insert into artista values(default,'Jimin',null,null,'05/03/70');
+insert into artista values(default, 'Caio Castro', null, 'BR', '15/07/75');
+insert into artista values(default, 'Larissa Manoella', null, 'BR', '15/07/75');
 
 insert into filme values(default,'Encontro Explosivo',2010,134,4,1);
 insert into filme values(default,'O Besouro Verde',2010,155,1,1);
@@ -73,6 +70,9 @@ insert into filme values(default,'Comer, Rezar, Amar',2010,177,2,1);
 insert into filme values(default,'Coringa',2019,122,6,1);
 insert into filme values(default,'Era uma vez em Hollywood',2020,119,4,2);
 insert into filme values(default,'Nasce uma estrela',2018,100,6,1);
+insert into filme values(default,'Uncharted',2010,134,4,1);
+insert into filme values(default,'O Besouro Azul',2010,155,1,1);
+insert into filme values(default,'Comer, Dormir, Viver',2010,177,2,1);
 
 insert into categoria values(default,'Aventura');
 insert into categoria values(default,'Romance');
@@ -80,10 +80,16 @@ insert into categoria values(default,'Comédia');
 insert into categoria values(default,'Ação');
 insert into categoria values(default,'Suspense');
 insert into categoria values(default,'Drama');
+insert into categoria values(default,'Tensão');
+insert into categoria values(default,'Terror');
+insert into categoria values(default,'Besteirol');
 
 insert into estudio values(default,'Paramount');
 insert into estudio values(default,'Disney');
 insert into estudio values(default,'Universal');
+insert into estudio values(default,'Marvel');
+insert into estudio values(default,'DC');
+insert into estudio values(default,'Pixar');
 
 insert into personagem values(1,1,'Natalie',10000);
 insert into personagem values(1,2,'Tom',10000);
@@ -91,38 +97,6 @@ insert into personagem values(5,3,'John',10000);
 insert into personagem values(3,2,'Ana',6000);
 insert into personagem values(6,5,'Tom',11000);
 insert into personagem values(4,4,'John',12000);
-
--- 6. Faça um select geral para verificar o conteúdo de cada uma das tabelas.
-
-select * from artista;
-select * from filme;
-select * from categoria;
-select * from estudio;
-select * from personagem;
-
--- 7. Em seguida, insira mais três registros (de sua autoria) em cada tabela.
-
-insert into artista values(default,'JungKook',null,'USA','15/07/75');
-insert into artista values(default,'V',null,'USA','20/08/67');
-insert into artista values(default,'Jimin',null,null,'05/03/70');
-
-insert into filme values(default,'Uncharted',2010,134,4,1);
-insert into filme values(default,'O Besouro Azul',2010,155,1,1);
-insert into filme values(default,'Comer, Dormir, Viver',2010,177,2,1);
-
-update filme set codfilme = 8
-where codfilme = 11;
-update filme set codfilme = 9
-where codfilme = 12
-
-insert into categoria values(default,'Tensão');
-insert into categoria values(default,'Terror');
-insert into categoria values(default,'Besteirol');
-
-insert into estudio values(default,'Marvel');
-insert into estudio values(default,'DC');
-insert into estudio values(default,'Pixar');
-
 insert into personagem values(1,3,'The Rock',10000);
 insert into personagem values(3,3,'Tin Tin',10000);
 insert into personagem values(5,4,'John Wick',10000);
