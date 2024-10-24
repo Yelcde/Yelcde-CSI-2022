ce6ab9c9103395ab3ccd08b44230bde2ce9ca93a
Johnner Yelcde <johnnercastrojy@gmail.com>
2024-10-24 17:15:20 -0230

    Pratica 1 IOC começando

---
+++ p5/pweb2/Pratica01 - IOC/spring-ioc/src/main/java/br/edu/ifpb/pweb2/spring_ioc/gerador/GeradorDocumento.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.springioc.gerador;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GeradorDocumento implements GeradorDocumentoIntf {
+	
+	@Override
+	public void addTexto(String texto) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ p5/pweb2/Pratica01 - IOC/spring-ioc/src/main/java/br/edu/ifpb/pweb2/spring_ioc/paragrafo/GeradorParagrafoIntf.java
@@ -0,0 +1,7 @@
+package br.edu.ifpb.pweb2.springioc.paragrafo;
+
+public interface GeradorParagrafoIntf {
+
+	public void addParagrafo(String texto);
+
+}
