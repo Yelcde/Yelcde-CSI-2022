b31d570939d5d52ae711bb56b2ebda40f74d03b2
Johnner Yelcde <johnnercastrojy@gmail.com>
2024-11-05 14:44:52 -0300

    Começando projeto BitBank - PWeb2

---
+++ p5/pweb2/bitbank/src/main/java/br/edu/ifpb/pweb2/bitbank/controller/CorrentistaController.java
@@ -0,0 +1,18 @@
+package br.edu.ifpb.pweb2.bitbank.controller;
+
+import br.edu.ifpb.pweb2.bitbank.model.Correntista;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/correntistas")
+public class CorrentistaController {
+
+    @RequestMapping("/form")
+    public String getForm(Correntista correntista, Model model) {
+        model.addAttribute("correntista", correntista);
+        return "correntistas/form";
+    }
+
+}
