ad8fb0567239654b8c0139cdad8f6a1b94f7ce40
yelcde <johnner.castro@assert.ifpb.edu.br>
2025-08-28 11:39:02 -0300

    Microservices - Parte 3

--- p6/Programação Distribuida/microservices/order/internal/application/core/api/api.go
+++ p6/Programação Distribuida/microservices/order/internal/application/core/api/api.go
@@ -30,3 +30,28 @@ func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 	}
 	return order, nil
 }
+
+func (a Application) Charge(ctx context.Context, payment domain.Payment) (domain.Payment, error) {
+	if payment.TotalPrice > 1000 {
+		return domain.Payment{}, status.Errorf(codes.InvalidArgument, "Payment over 1000 is not allowed.")
+	}
+	err := a.db.Save(ctx, &payment)
+	if err != nil {
+		return domain.Payment{}, err
+	}
+	return payment, nil
+}
+
+func (a Adapter) Create(ctx context.Context, request *payment.CreatePaymentRequest) (*payment.CreatePayment-Response, error) {
+	log.WithContext(ctx).Info("Creating payment...")
+
+	newPayment := domain.NewPayment(request.UserId, request.OrderId, request.TotalPrice)
+	result, err := a.api.Charge(ctx, newPayment)
+	code := status.Code(err)
+	if code == codes.InvalidArgument {
+		return nil, err
+	} else if err != nil {
+		return nil, status.New(codes.Internal, fmt.Sprintf("failed to charge. %v ", err)).Err()
+	}
+	return &payment.CreatePaymentResponse{PaymentId: result.ID}, nil
+}
